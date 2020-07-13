ae151085c25cf27c5a2a3604b8fb5d6cde8530a2
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-07-12 22:50:59 -0400

    Initial Commit

---
+++ BasicCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicCalculator
+//
+//  Created by Anthony Hopkins on 2020-07-12.
+//  Copyright © 2020 Anthony Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
